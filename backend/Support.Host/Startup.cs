d7350e47eed1dc985ace309c1bde68afae2c9d10
Elahe HosseinZadeh <e.hosseinzadeh73@gmail.com>
2019-09-11 17:14:24 +0430

    framework

--- backend/Support.Host/Startup.cs
+++ backend/Support.Host/Startup.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Text;
 using Autofac;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using Support.Config;
 using Support.Host.Settings;
 using Swashbuckle.AspNetCore.Swagger;
@@ -25,9 +24,21 @@ namespace Support.Host
 
         public void ConfigureServices(IServiceCollection services)
         {
-            // services.AddDbContext<SalesContext>();
 
-
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+   .AddJwtBearer(options =>
+   {
+       options.TokenValidationParameters = new TokenValidationParameters
+       {
+           ValidateIssuer = true,
+           ValidateAudience = true,
+           ValidateLifetime = true,
+           ValidateIssuerSigningKey = true,
+           ValidIssuer = Configuration["Jwt:Issuer"],
+           ValidAudience = Configuration["Jwt:Issuer"],
+           IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+       };
+   });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddSwaggerGen(c =>
@@ -62,6 +73,8 @@ namespace Support.Host
             {
                 c.SwaggerEndpoint("/swagger/v1.0/swagger.json", "Support API (V 1.0)");
             });
+            app.UseMiddleware<AuthenticationMiddleware>();
+
             app.UseMvcWithDefaultRoute();
 
             //if (env.IsDevelopment())
